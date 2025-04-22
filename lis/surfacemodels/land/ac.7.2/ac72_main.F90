2a02158150b81b2bf15d841ca917970c4f8991b9
lbusschaert <louise.busschaert@kuleuven.be>
2025-04-22 16:36:13 +0200

    ENH: solved remaining irun calls

--- lis/surfacemodels/land/ac.7.2/ac72_main.F90
+++ lis/surfacemodels/land/ac.7.2/ac72_main.F90
@@ -914,7 +914,7 @@ subroutine AC72_main(n)
                start_day_t = ac72_search_start_Temp(time1days,time2days,AC72_struc(n)%crit_window, &
                                                 AC72_struc(n)%Temp_crit_tmin, AC72_struc(n)%Temp_crit_days, &
                                                 AC72_struc(n)%Temp_crit_occurrence, AC72_struc(n)%ac72(t)%Tmin_record)
-               call set_project_input(AC72_struc(n)%ac72(t)%irun, 'Crop_Day1', start_day_t)
+               call set_project_input(AC72_struc(n)%irun, 'Crop_Day1', start_day_t)
            endif
 
            if (AC72_struc(n)%Rainfall_crit) then
@@ -930,11 +930,11 @@ subroutine AC72_main(n)
                start_day_p = ac72_search_start_Rainfall(time1days,time2days,AC72_struc(n)%crit_window, &
                                                 AC72_struc(n)%Rainfall_crit_amount, AC72_struc(n)%Rainfall_crit_days, &
                                                 AC72_struc(n)%Rainfall_crit_occurrence, AC72_struc(n)%ac72(t)%pcp_record)
-               call set_project_input(AC72_struc(n)%ac72(t)%irun, 'Crop_Day1', start_day_p)
+               call set_project_input(AC72_struc(n)%irun, 'Crop_Day1', start_day_p)
            endif
 
            if (AC72_struc(n)%Temp_crit.and.AC72_struc(n)%Rainfall_crit) then
-               call set_project_input(AC72_struc(n)%ac72(t)%irun, 'Crop_Day1', max(start_day_t, start_day_t))
+               call set_project_input(AC72_struc(n)%irun, 'Crop_Day1', max(start_day_t, start_day_t))
            endif
 
            ! Set Global variable to pass T record to AquaCrop
